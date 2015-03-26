656c549adb284b42481db3039e39275f586071e4
Priit Tark <priit@gitlab.eu>
2015-03-26 17:03:25 +0200

    Include main app application

--- app/controllers/depp/application_controller.rb
+++ app/controllers/depp/application_controller.rb
@@ -1,7 +1,5 @@
 module Depp
-  class ApplicationController < ActionController::Base
-    include CurrentUserHelper
-
+  class ApplicationController < ::ApplicationController
     # Prevent CSRF attacks by raising an exception.
     # For APIs, you may want to use :null_session instead.
     protect_from_forgery with: :exception
