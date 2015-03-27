272e1cc85e19de1943f9724d99d5db8bc17e2f0d
Priit Tark <priit@gitlab.eu>
2015-03-27 15:44:18 +0200

    fixing total pages issue

--- app/controllers/depp/contacts_controller.rb
+++ app/controllers/depp/contacts_controller.rb
@@ -6,8 +6,8 @@ module Depp
       res = depp_current_user.repp_request('contacts', { page: params[:page] })
       flash.now[:epp_results] = [{ 'code' => res.code, 'msg' => res.message }]
       @response = res.parsed_body.with_indifferent_access if res.code == '200'
-      @contacts    = @response[:contacts] if @response
-      @total_pages = @response[:total_pages] if @response
+      @contacts    = @response ? @response[:contacts] : []
+      @total_pages = @response ? @response[:total_pages] : 0
     end
 
     def new
