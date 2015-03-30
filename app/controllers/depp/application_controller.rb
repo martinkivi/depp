592e57a6965be64ad4215334464d6dd690410583
Martin Lensment <mlensment@gmail.com>
2015-03-30 16:22:20 +0300

    Add contact pagination and auth checks

--- app/controllers/depp/application_controller.rb
+++ app/controllers/depp/application_controller.rb
@@ -14,7 +14,29 @@ module Depp
       redirect_to login_url, alert: t(:no_connection_to_registry)
     end
 
+    before_action :authenticate_user
+    def authenticate_user
+      if ENV['session_timeout']
+        redirect_to main_app.login_url and return unless depp_current_user && session[:last_seen]
+
+        if (session[:last_seen].to_i + ENV['session_timeout'].to_i) < Time.now.to_i
+          session_timeout
+        else
+          session[:last_seen] = Time.now.to_i
+        end
+      else
+        redirect_to main_app.login_url and return unless depp_current_user
+      end
+    end
+
+    def session_timeout
+      reset_session
+      flash[:alert] = t('your_session_has_timed_out')
+      redirect_to main_app.login_url and return
+    end
+
     def depp_current_user
+      return nil unless current_user
       @depp_current_user ||= Depp::User.new(
         tag: current_user.username,
         password: current_user.password
